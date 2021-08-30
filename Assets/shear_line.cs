d3ca9d2fef5365af9bed756f0080c871f999138f
sweedp <hendrik-timm@hotmail.de>
2021-08-30 19:52:46 +0200

    change pin prefab, add script shearline

--- Assets/shear_line.cs
+++ Assets/shear_line.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class shear_line : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private GameObject cylinder;
+    private GameObject key_pin;
+    private GameObject driver_pin;
+    private Vector3 scaleChange, positionChange;
+
+
     void Start()
     {
+        if(key_pin==null)
+            key_pin = GameObject.FindGameObjectWithTag("Key Pin");
+        if (driver_pin == null)
+            driver_pin = GameObject.FindGameObjectWithTag("Driver Pin");
+
+
+        Camera.main.clearFlags = CameraClearFlags.SolidColor;
+
+        key_pin.transform.position = new Vector3(0, 0, 0);
+
+
         
+
+
+
+
+        scaleChange = new Vector3(-0.01f, -0.01f, -0.01f);
+        positionChange = new Vector3(0.0f, -0.005f, 0.0f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //cylinder.transform.localScale += scaleChange;
+        //cylinder.transform.position += positionChange;
+
+        // Move upwards when the sphere hits the floor or downwards
+        // when the sphere scale extends 1.0f.
+        //if (cylinder.transform.localScale.y < 0.1f || cylinder.transform.localScale.y > 1.0f)
+        //{
+        //    scaleChange = -scaleChange;
+          //  positionChange = -positionChange;
+       // }
     }
+
+    
 }
