830e9b51df31d58dce20c490302c932e6b867a97
sweedp <hendrik-timm@hotmail.de>
2021-08-30 22:42:56 +0200

    pins with script for different sizes

--- Assets/shear_line.cs
+++ Assets/shear_line.cs
@@ -5,23 +5,50 @@ using UnityEngine;
 public class shear_line : MonoBehaviour
 {
 
-    private GameObject cylinder;
-    private GameObject key_pin;
-    private GameObject driver_pin;
+
+    public float max_height = 7; // 3 spiral, 2x2 pins
+    public float key_pin_height = 2;
+    public float driver_pin_height = 2;
+    public float spring_height = 3;
+
+    private float key_pin_scale_y;
+    private float driver_pin_scale_y;
+    private float key_pin_pos_y;
+    private float driver_pin_pos_y;
+    public GameObject key_pin;
+    public GameObject driver_pin;
+    private GameObject spring;
     private Vector3 scaleChange, positionChange;
 
 
     void Start()
     {
         if(key_pin==null)
-            key_pin = GameObject.FindGameObjectWithTag("Key Pin");
+            //key_pin = GameObject.FindGameObjectWithTag("Key Pin");
         if (driver_pin == null)
-            driver_pin = GameObject.FindGameObjectWithTag("Driver Pin");
+           // driver_pin = GameObject.FindGameObjectWithTag("Driver Pin");
+        if (spring == null)
+            spring = GameObject.FindGameObjectWithTag("Spring");
+
+        // scale of key_pin and driver_pin need to add up to 2.
+        // minimal scale of one pin should be between 0.5 - 1.5
+        // key_pin_scale
+
+
+
+        key_pin_scale_y = Random.Range(0.5f, 1.5f);
+        driver_pin_scale_y = 2 - key_pin_scale_y;
+
+        key_pin_pos_y = 6- key_pin_scale_y;
+        driver_pin_pos_y = 2 + driver_pin_scale_y;
 
 
         Camera.main.clearFlags = CameraClearFlags.SolidColor;
-
-        key_pin.transform.position = new Vector3(0, 0, 0);
+        
+        key_pin.transform.position = new Vector3(key_pin.transform.position.x, key_pin_pos_y, key_pin.transform.position.z);
+        driver_pin.transform.position = new Vector3(driver_pin.transform.position.x, driver_pin_pos_y, driver_pin.transform.position.z);
+        key_pin.transform.localScale = new Vector3(1, key_pin_scale_y, 1);
+        driver_pin.transform.localScale = new Vector3(1, driver_pin_scale_y, 1);
 
 
         
